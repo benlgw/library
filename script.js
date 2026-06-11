01a3b3493dd329427d4e893f43dd8d00f6420a93
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-11 13:11:45 +0100

    create function for adding books and added some placeholders

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-function Book(name, author, release, totalPages, read) {
+function Book(id, name, author, release, totalPages, read) {
 	if (!new.target) {
 		throw Error("You must use the 'new' operator to call the constructor");
 	}
 
+	this.id = id;
 	this.name = name;
 	this.author = author;
 	this.release = release;
 	this.totalPages = +totalPages;
-	this.upTo = Math.floor(this.totalPages * Math.random());
 	this.read = read;
+	this.read == true
+		? (this.upTo = this.totalPages)
+		: Math.floor(this.totalPages * Math.random());
 }
 
 Book.prototype.info = function () {
 	return `${this.name} by ${this.author} released in ${this.release}. You are up to page ${this.upTo} out of ${this.totalPages}`;
 };
 
-// const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 1937, 295, false);
+const books = [];
 
-// console.log(hobbit.info());
-// console.log(hobbit.valueOf());
+function createBook({ book, author, release, totalPages, read }) {
+	books.push(
+		new Book(crypto.randomUUID(), book, author, release, totalPages, read),
+	);
+}
+
+// Create PlaceHolder Books Here
+
+createBook({
+	book: "The Innocent Man",
+	author: "John Grisham",
+	release: 2006,
+	totalPages: 368,
+	read: true,
+});
+
+createBook({
+	book: "Harry Potter: Philosopher's Stone",
+	author: "J.K. Rowling",
+	release: 1997,
+	totalPages: 223,
+	read: false,
+});
+
+createBook({
+	book: "Harry Potter: Philosopher's Stone",
+	author: "J.K. Rowling",
+	release: 1997,
+	totalPages: 223,
+	read: false,
+});
+
+// End PlaceHolder Books
+
+const bookGrid = document.querySelector("body");
+
+books.forEach((book) => {
+	const card = document.createElement("div");
+	card.classList.add("card");
+	card.setAttribute("id", book.id);
+	card.textContent = book.info();
+	bookGrid.append(card);
+});
